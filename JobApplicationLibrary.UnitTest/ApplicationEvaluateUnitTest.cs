ae58197f7fc36d95ab443b23dba2eed60c788e8a
Kanan Huseynov <info.kennans26@gmail.com>
2024-03-03 13:10:14 +0400

    Unit Test #2 | Let's develop our test scenarios

--- JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
+++ JobApplicationLibrary.UnitTest/ApplicationEvaluateUnitTest.cs
@@ -24,5 +24,48 @@ namespace JobApplicationLibrary.UnitTest
             // Assert
             Assert.AreEqual(appResult, ApplicationResult.AutoRejected);
         }
+
+        [Test]
+        public void Application_WithNoTechStack_TransferredToAutoRejected()
+        {
+            // Arrange
+            var evaluator = new ApplicationEvaluator();
+            var form = new JobApplication()
+            {
+                Applicant = new Applicant()
+                {
+                    Age = 19
+                },
+                TechStackList = new List<string> { "" }
+            };
+
+            // Action
+            var appResult = evaluator.Evaluate(form);
+
+            // Assert
+            Assert.AreEqual(appResult, ApplicationResult.AutoRejected);
+        }
+
+        [Test]
+        public void Application_WithTechStackOver75P_TransferredToAutoAccepted()
+        {
+            // Arrange
+            var evaluator = new ApplicationEvaluator();
+            var form = new JobApplication()
+            {
+                Applicant = new Applicant()
+                {
+                    Age = 19
+                },
+                TechStackList = new List<string> { "C#", "RabbitMQ", "Microservice", "Visual Studio" },
+                YearsOfExperience = 16
+            };
+
+            // Action
+            var appResult = evaluator.Evaluate(form);
+
+            // Assert
+            Assert.AreEqual(appResult, ApplicationResult.AutoAccepted);
+        }
     }
 }
--- JobApplicationLibrary/ApplicationEvaluator.cs
+++ JobApplicationLibrary/ApplicationEvaluator.cs
@@ -1,16 +1,48 @@
 ﻿using JobApplicationLibrary.Models;
+using JobApplicationLibrary.Services;
 
 namespace JobApplicationLibrary
 {
     public class ApplicationEvaluator
     {
         private const int minAge = 18;
+        private const int autoAcceptedYearOfExperience = 15;
+        private List<string> techStackList = new() { "C#", "RabbitMQ", "Microservice", "Visual Studio" };
+        private IdentityValidator identityValidator;
+
+        public ApplicationEvaluator()
+        {
+            identityValidator = new IdentityValidator();
+        }
+
         public ApplicationResult Evaluate(JobApplication form)
         {
             if (form.Applicant.Age < minAge)
                 return ApplicationResult.AutoRejected;
 
+            var validIdentity = identityValidator.IsValid(form.Applicant.IdentityNumber);
+
+            if (!validIdentity)
+                return ApplicationResult.TransferredToHR;
+
+            var sr = GetTechStackSimilarityRate(form.TechStackList);
+
+            if (sr < 25)
+                return ApplicationResult.AutoRejected;
+            if (sr > 75 && form.YearsOfExperience >= autoAcceptedYearOfExperience)
+                return ApplicationResult.AutoAccepted;
+
             return ApplicationResult.AutoAccepted;
+        }
+
+        private int GetTechStackSimilarityRate(List<string> techStacks)
+        {
+            var matchedCount =
+                techStacks
+                .Where(i => techStackList.Contains(i, StringComparer.OrdinalIgnoreCase))
+                .Count();
+
+            return (int)((double)matchedCount / techStackList.Count) * 100;
         }
     }
 
